3a10b685396c04bbc4a791526adf288701afc14c
Igor Eskin <eskinhd@gmail.com>
2018-07-19 13:02:21 -0400

    Class functions of class LinkedList created

--- swap.js
+++ swap.js
@@ -9,4 +9,70 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-}  
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  getLast() {
+    let node = this.head;
+    while (node && node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  insertAt(data, index) {
+    let node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+      return;
+    }
+    if (index >= this.size()) {
+      this.getLast().next = node;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    node.next = previous.next;
+    previous.next = node;
+    return;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    previous && previous.next ? previous.next = previous.next.next : null;
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+
+
+}
